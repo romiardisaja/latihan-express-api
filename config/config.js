49c64070c0f89eaeff9d95a5aabdf28cc4123499
romiardisaja <romiardisaja@gmail.com>
2022-09-06 22:52:16 +0700

    Set up sequelize

--- config/config.js
+++ config/config.js
@@ -10,24 +10,24 @@ const {
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
   }
 }
