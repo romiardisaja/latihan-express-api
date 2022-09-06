eb8b3800690d170080f1e1934937f9be8b1bbca7
romiardisaja <romiardisaja@gmail.com>
2022-09-06 22:44:25 +0700

    membuat project baru

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOSTNAME,
+  DB_NAME,
+  DB_DIALECT
+} = process.env;
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
