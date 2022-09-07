ebd83490d20dfdc73bae11794d1b3a5bee4bb705
romiardisaja <romiardisaja@gmail.com>
2022-09-07 09:32:20 +0700

    Menambahkan model Product

---
+++ models/Product.js
@@ -0,0 +1,33 @@
+module.exports = (DataTypes, DataTypes) => {
+    const Product = DataTypes.define('Produk', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+           }, 
+           name: {
+            type: DataTypes.STRING,
+            allowNull: false,        
+           },
+           brand: {
+            type: DataTypes.STRING,
+            allowNull: false
+           },
+           description: {
+            type: DataTypes.TEXT
+           },
+           createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+           },
+           updateddAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+           }
+    }, {
+        tableName: 'products'
+    })
+
+    return Product;
+}
